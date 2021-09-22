34226a7eda0d3000c24953f1c456905eeb756910
ds-grybanov <grybanovdavid@gmail.com>
2021-09-22 15:55:42 +0300

    initial_proj

---
+++ app/js/main.js
@@ -0,0 +1,590 @@
+
+  jQuery(function() {
+	initSlickCarousel();
+	initAccordion();
+	initFormValidation();
+});
+
+$(document).ready(function(){
+	$('.header__burger-menu').click(function(event){
+	  $('.header__burger-menu, .header__menu, .burger__menu-line').toggleClass('active');
+	  $('body').toggleClass('lock');
+	});
+});
+
+$(document).ready(function() {
+	$('.nice-select').niceSelect();
+  });
+
+
+  $('.steps__list-item').matchHeight({
+    byRow: false,
+    property: 'height',
+    target: null,
+    remove: false
+});
+// slick init
+function initSlickCarousel() {
+	jQuery('.reviews__slider').slick({
+		slidesToScroll: 1,
+		slideToShow: 1,
+		autoplay: true,
+        autoplaySpeed: 2000,
+		prevArrow: '.prev-button-slick',
+		nextArrow: '.next-button-slick'
+	});
+	var helpers = {
+		addZeros: function (n) {
+			return (n < 10) ? '0' + n : '' + n;
+		}
+	};
+	$(this).on('afterChange', function(event, slick, currentSlide){
+		$('.item-counter').html(helpers.addZeros(currentSlide + 1));
+		});
+    // $('.reviews__slider').on('afterChange', function(event, slick, currentSlide){
+    //     $(".counter").text(currentSlide + 1);
+    //  });
+};
+
+function initFormValidation() {
+	jQuery('.inner__form').formValidation({
+		errorClass: 'input-error',
+		successClass: 'input-succes'
+	});
+}
+
+
+
+// accordion menu init
+function initAccordion() {
+	jQuery('.questions__accordion').slideAccordion({
+		opener: '.accordion-item-title',
+		slider: '.accordion-item-inner',
+		animSpeed: 300
+	});
+}
+
+/*
+ * jQuery Accordion plugin new
+ */
+;(function(root, factory) {
+
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('jquery'));
+	} else {
+		root.SlideAccordion = factory(jQuery);
+	}
+}(this, function($) {
+
+	'use strict';
+	var accHiddenClass = 'js-acc-hidden';
+
+	function SlideAccordion(options) {
+		this.options = $.extend(true, {
+			allowClickWhenExpanded: false,
+			activeClass:'active',
+			opener:'.opener',
+			slider:'.slide',
+			animSpeed: 300,
+			collapsible:true,
+			event: 'click',
+			scrollToActiveItem: {
+				enable: false,
+				breakpoint: 767, // max-width
+				animSpeed: 600,
+				extraOffset: null
+			}
+		}, options);
+		this.init();
+	}
+
+	SlideAccordion.prototype = {
+		init: function() {
+			if (this.options.holder) {
+				this.findElements();
+				this.setStateOnInit();
+				this.attachEvents();
+				this.makeCallback('onInit');
+			}
+		},
+
+		findElements: function() {
+			this.$holder = $(this.options.holder).data('SlideAccordion', this);
+			this.$items = this.$holder.find(':has(' + this.options.slider + ')');
+		},
+
+		setStateOnInit: function() {
+			var self = this;
+
+			this.$items.each(function() {
+				if (!$(this).hasClass(self.options.activeClass)) {
+					$(this).find(self.options.slider).addClass(accHiddenClass);
+				}
+			});
+		},
+
+		attachEvents: function() {
+			var self = this;
+
+			this.accordionToggle = function(e) {
+				var $item = jQuery(this).closest(self.$items);
+				var $actiItem = self.getActiveItem($item);
+
+				if (!self.options.allowClickWhenExpanded || !$item.hasClass(self.options.activeClass)) {
+					e.preventDefault();
+					self.toggle($item, $actiItem);
+				}
+			};
+
+			this.$items.on(this.options.event, this.options.opener, this.accordionToggle);
+		},
+
+		toggle: function($item, $prevItem) {
+			if (!$item.hasClass(this.options.activeClass)) {
+				this.show($item);
+			} else if (this.options.collapsible) {
+				this.hide($item);
+			}
+
+			if (!$item.is($prevItem) && $prevItem.length) {
+				this.hide($prevItem);
+			}
+
+			this.makeCallback('beforeToggle');
+		},
+
+		show: function($item) {
+			var $slider = $item.find(this.options.slider);
+
+			$item.addClass(this.options.activeClass);
+			$slider.stop().hide().removeClass(accHiddenClass).slideDown({
+				duration: this.options.animSpeed,
+				complete: function() {
+					$slider.removeAttr('style');
+					if (
+						this.options.scrollToActiveItem.enable &&
+						window.innerWidth <= this.options.scrollToActiveItem.breakpoint
+					) {
+						this.goToItem($item);
+					}
+					this.makeCallback('onShow', $item);
+				}.bind(this)
+			});
+
+			this.makeCallback('beforeShow', $item);
+		},
+
+		hide: function($item) {
+			var $slider = $item.find(this.options.slider);
+
+			$item.removeClass(this.options.activeClass);
+			$slider.stop().show().slideUp({
+				duration: this.options.animSpeed,
+				complete: function() {
+					$slider.addClass(accHiddenClass);
+					$slider.removeAttr('style');
+					this.makeCallback('onHide', $item);
+				}.bind(this)
+			});
+
+			this.makeCallback('beforeHide', $item);
+		},
+
+		goToItem: function($item) {
+			var itemOffset = $item.offset().top;
+
+			if (itemOffset < $(window).scrollTop()) {
+				// handle extra offset
+				if (typeof this.options.scrollToActiveItem.extraOffset === 'number') {
+					itemOffset -= this.options.scrollToActiveItem.extraOffset;
+				} else if (typeof this.options.scrollToActiveItem.extraOffset === 'function') {
+					itemOffset -= this.options.scrollToActiveItem.extraOffset();
+				}
+
+				$('body, html').animate({
+					scrollTop: itemOffset
+				}, this.options.scrollToActiveItem.animSpeed);
+			}
+		},
+
+		getActiveItem: function($item) {
+			return $item.siblings().filter('.' + this.options.activeClass);
+		},
+
+		makeCallback: function(name) {
+			if (typeof this.options[name] === 'function') {
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				this.options[name].apply(this, args);
+			}
+		},
+
+		destroy: function() {
+			this.$holder.removeData('SlideAccordion');
+			this.$items.off(this.options.event, this.options.opener, this.accordionToggle);
+			this.$items.removeClass(this.options.activeClass).each(function(i, item) {
+				$(item).find(this.options.slider).removeAttr('style').removeClass(accHiddenClass);
+			}.bind(this));
+			this.makeCallback('onDestroy');
+		}
+	};
+
+	$.fn.slideAccordion = function(opt) {
+		var args = Array.prototype.slice.call(arguments);
+		var method = args[0];
+
+		return this.each(function() {
+			var $holder = jQuery(this);
+			var instance = $holder.data('SlideAccordion');
+
+			if (typeof opt === 'object' || typeof opt === 'undefined') {
+				new SlideAccordion($.extend(true, {
+					holder: this
+				}, opt));
+			} else if (typeof method === 'string' && instance) {
+				if(typeof instance[method] === 'function') {
+					args.shift();
+					instance[method].apply(instance, args);
+				}
+			}
+		});
+	};
+
+	(function() {
+		var tabStyleSheet = $('<style type="text/css">')[0];
+		var tabStyleRule = '.' + accHiddenClass;
+		tabStyleRule += '{position:absolute !important;left:-9999px !important;top:-9999px !important;display:block !important; width: 100% !important;}';
+		if (tabStyleSheet.styleSheet) {
+			tabStyleSheet.styleSheet.cssText = tabStyleRule;
+		} else {
+			tabStyleSheet.appendChild(document.createTextNode(tabStyleRule));
+		}
+		$('head').append(tabStyleSheet);
+	}());
+
+	return SlideAccordion;
+}));
+
+
+/*
+ * jQuery form validation plugin
+ */
+;(function($) {
+	'use strict';
+
+	var FormValidation = (function() {
+		var Validator = function($field, $fields) {
+			this.$field = $field;
+			this.$fields = $fields;
+		};
+
+		Validator.prototype = {
+			reg: {
+				email: '^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$',
+				number: '^[0-9]+$'
+			},
+
+			checkField: function() {
+				return {
+					state: this.run(),
+					$fields: this.$field.add(this.additionalFields)
+				}
+			},
+
+			run: function() {
+				var fieldType;
+
+				switch (this.$field.get(0).tagName.toUpperCase()) {
+					case 'SELECT':
+						fieldType = 'select';
+						break;
+
+					case 'TEXTAREA':
+						fieldType = 'text';
+						break;
+
+					default:
+						fieldType = this.$field.data('type') || this.$field.attr('type');
+				}
+
+				var functionName = 'check_' + fieldType.replace(/-/g,'_');
+				var state = true;
+
+				if ($.isFunction(this[functionName])) {
+					state = this[functionName]();
+
+					if (state && this.$field.data('confirm')) {
+						state = this.check_confirm();
+					}
+				}
+
+				return state;
+			},
+
+			check_email: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+				var requiredOrValue = required || value.length;
+
+				if ((requiredOrValue && !this.check_regexp(value, this.reg.email))) {
+					return false;
+				}
+
+				return requiredOrValue ? true : null;
+			},
+
+			check_number: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+				var isNumber = this.check_regexp(value, this.reg.number);
+				var requiredOrValue = required || value.length;
+
+				if (requiredOrValue && !isNumber) {
+					return false;
+				}
+
+				var min = this.$field.data('min');
+				var max = this.$field.data('max');
+				value = +value;
+
+				if ((min && (value < min || !isNumber)) || (max && (value > max || !isNumber))) {
+					return false;
+				}
+
+				return (requiredOrValue || min || max) ? true : null;
+			},
+
+			check_password: function() {
+				return this.check_text();
+			},
+
+			check_text: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+
+				if (this.$field.data('required') && !value.length) {
+					return false;
+				}
+
+				var min = +this.$field.data('min');
+				var max = +this.$field.data('max');
+
+				if ((min && value.length < min) || (max && value.length > max)) {
+					return false;
+				}
+
+				var regExp = this.$field.data('regexp');
+
+				if (regExp && !this.check_regexp(value, regExp)) {
+					return false;
+				}
+
+				return (required || min || max || regExp) ? true : null;
+			},
+
+			check_confirm: function() {
+				var value = this.getValue();
+				var $confirmFields = this.$fields.filter('[data-confirm="' + this.$field.data('confirm') + '"]');
+				var confirmState = true;
+
+				for (var i = $confirmFields.length - 1; i >= 0; i--) {
+					if ($confirmFields.eq(i).val() !== value || !value.length) {
+						confirmState = false;
+						break;
+					}
+				}
+
+				this.additionalFields = $confirmFields;
+
+				return confirmState;
+			},
+
+			check_select: function() {
+				var required = this.$field.data('required');
+
+				if (required && this.$field.get(0).selectedIndex === 0) {
+					return false;
+				}
+
+				return required ? true : null;
+			},
+
+			check_radio: function() {
+				var $fields = this.$fields.filter('[name="' + this.$field.attr('name') + '"]');
+				var required = this.$field.data('required');
+
+				if (required && !$fields.filter(':checked').length) {
+					return false;
+				}
+
+				this.additionalFields = $fields;
+
+				return required ? true : null;
+			},
+
+			check_checkbox: function() {
+				var required = this.$field.data('required');
+
+				if (required && !this.$field.prop('checked')) {
+					return false;
+				}
+
+				return required ? true : null;
+			},
+
+			check_at_least_one: function() {
+				var $fields = this.$fields.filter('[data-name="' + this.$field.data('name') + '"]');
+
+				if (!$fields.filter(':checked').length) {
+					return false;
+				}
+
+				this.additionalFields = $fields;
+
+				return true;
+			},
+
+			check_regexp: function(val, exp) {
+				return new RegExp(exp).test(val);
+			},
+
+			getValue: function() {
+				if (this.$field.data('trim')) {
+					this.$field.val($.trim(this.$field.val()));
+				}
+
+				return this.$field.val();
+			}
+		};
+
+		var publicClass = function(form, options) {
+			this.$form = $(form).attr('novalidate', 'novalidate');
+			this.options = options;
+		};
+
+		publicClass.prototype = {
+			buildSelector: function(input) {
+				return input + ':not(' + this.options.skipDefaultFields + (this.options.skipFields ? ',' + this.options.skipFields : '') + ')';
+			},
+
+			init: function() {
+				this.fieldsSelector = this.buildSelector(':input');
+
+				this.$form
+					.on('submit', this.submitHandler.bind(this))
+					.on('keyup blur', this.fieldsSelector, this.changeHandler.bind(this))
+					.on('change', this.buildSelector('select'), this.changeHandler.bind(this))
+					.on('focus', this.fieldsSelector, this.focusHandler.bind(this));
+			},
+
+			submitHandler: function(e) {
+				var self = this;
+				var $fields = this.getFormFields();
+
+				this.getClassTarget($fields)
+					.removeClass(this.options.errorClass + ' ' + this.options.successClass);
+
+				this.setFormState(true);
+
+				$fields.each(function(i, input) {
+					var $field = $(input);
+					var $classTarget = self.getClassTarget($field);
+
+					// continue iteration if $field has error class already
+					if ($classTarget.hasClass(self.options.errorClass)) {
+						return;
+					}
+
+					self.setState(new Validator($field, $fields).checkField());
+				});
+
+				return this.checkSuccess($fields, e);
+			},
+
+			checkSuccess: function($fields, e) {
+				var self = this;
+				var success = this.getClassTarget($fields || this.getFormFields())
+								  .filter('.' + this.options.errorClass).length === 0;
+
+				if (e && success && this.options.successSendClass) {
+					e.preventDefault();
+
+					$.ajax({
+						url: this.$form.removeClass(this.options.successSendClass).attr('action') || '/',
+						type: this.$form.attr('method') || 'POST',
+						data: this.$form.serialize(),
+						success: function() {
+							self.$form.addClass(self.options.successSendClass);
+						}
+					});
+				}
+
+				this.setFormState(success);
+
+				return success;
+			},
+
+			changeHandler: function(e) {
+				var $field = $(e.target);
+
+				if ($field.data('interactive')) {
+					this.setState(new Validator($field, this.getFormFields()).checkField());
+				}
+
+				this.checkSuccess();
+			},
+
+			focusHandler: function(e) {
+				var $field = $(e.target);
+
+				this.getClassTarget($field)
+					.removeClass(this.options.errorClass + ' ' + this.options.successClass);
+
+				this.checkSuccess();
+			},
+
+			setState: function(result) {
+				this.getClassTarget(result.$fields)
+					.toggleClass(this.options.errorClass, result.state !== null && !result.state)
+					.toggleClass(this.options.successClass, result.state !== null && this.options.successClass && !!result.state);
+			},
+
+			setFormState: function(state) {
+				if (this.options.errorFormClass) {
+					this.$form.toggleClass(this.options.errorFormClass, !state);
+				}
+			},
+
+			getClassTarget: function($input) {
+				return (this.options.addClassToParent ? $input.closest(this.options.addClassToParent) : $input);
+			},
+
+			getFormFields: function() {
+				return this.$form.find(this.fieldsSelector);
+			}
+		};
+
+		return publicClass;
+	}());
+
+	$.fn.formValidation = function(options) {
+		options = $.extend({}, {
+			errorClass: 'input-error',
+			successClass: '',
+			errorFormClass: '',
+			addClassToParent: '',
+			skipDefaultFields: ':button, :submit, :image, :hidden, :reset',
+			skipFields: '',
+			successSendClass: ''
+		}, options);
+
+		return this.each(function() {
+			new FormValidation(this, options).init();
+		});
+	};
+}(jQuery));
+
+
